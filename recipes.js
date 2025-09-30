b0807d991b604d64ffad34adcc892c4c37ff746d
Micah Knox <miknox0627@gmail.com>
2025-09-30 11:11:44 -0500

    ingredients added got json working

--- recipes.js
+++ recipes.js
@@ -9,5 +9,41 @@ const recipeName = params.get('recipe');
 
 
 
-const myElement = document.getElementById("recipe-title");
-    myElement.textContent = `${recipeName}`;
+fetch('recipes.json')
+.then(response => response.json())
+.then(data => {
+    //title
+    const title = document.getElementById("recipe-title");
+    title.textContent = `${data[recipeName].title}`;
+
+    // const pic = document.getElementById("picture");
+    // pic.src = `${data[recipeName].image}`;
+    
+    //ingredients
+    const ingredientsList = document.getElementById("ingredients-table");
+    ["Ingredient","Amount"].forEach(text => {
+    const th = document.createElement("th");
+    th.textContent = text;
+    ingredientsList.appendChild(th);
+    });
+    data[recipeName].ingredients.forEach(ingredient => {
+    const row = document.createElement("tr");
+
+    const amountCell = document.createElement("td");
+    amountCell.textContent = ingredient.amount;
+
+    const itemCell = document.createElement("td");
+    itemCell.textContent = ingredient.item;
+    row.appendChild(itemCell);
+    row.appendChild(amountCell);
+    
+
+    ingredientsList.appendChild(row);    
+    });
+
+
+    
+
+
+}).catch(error => console.error('Failed to fetch data:', error)); 
+console.log(recipeName);
